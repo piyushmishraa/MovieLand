2b1307e552694726192e3ad732824bcd8f251662
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-05-17 17:31:21 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 
 
 
  const App=()=> {
   const[counter,setcounter]=useState(0);
+
+  useEffect(()=>{
+    alert("counter is "+ counter)
+  },[counter]);
+
   return (
     <div className='App'>
     <button onClick={()=>{setcounter((prevCount)=>prevCount - 1)}}>-</button>
