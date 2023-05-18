f4253b9a3913ba25a9fe90c1dbfa067089b62669
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-05-18 07:07:23 +0530

    fetched omdb api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+//524730dd
+const URL ="http://www.omdbapi.com?apikey=524730dd"
 
 const App= ()=>{
+       const searchMovies=async(title)=>{
+       const response=await fetch(`${URL}&s=${title}`) 
+       const data=await response.json();
+       
+       console.log(data.Search);
+    } 
+
+
+    useEffect(()=>{
+      searchMovies('ironman');
+    },[])
     return(
         <h1>hello,react</h1>
     );
